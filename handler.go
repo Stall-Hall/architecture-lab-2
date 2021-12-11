8b2318145056d0a0fca22a71fc4f6a87bc758063
suhanovamasha21@gmail.com <suhanovamasha21@gmail.com>
2021-12-11 20:27:27 +0200

    Little fix

--- handler.go
+++ handler.go
@@ -5,30 +5,28 @@ import (
 	"io/ioutil"
 )
 
+// ComputeHandler should be constructed with input io.Reader and output io.Writer.
+// Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
 		Input  io.Reader
 		Output io.Writer
 }
 
-func (ch *ComputeHandler) Compute() error 
-{
+func (ch *ComputeHandler) Compute() error {
 	bytes, readErr := ioutil.ReadAll(ch.Input)
-	if readErr != nil 
-	{
+	if readErr != nil {
 		return readErr
 	}
 
 	e := string(bytes)
 
 	_, writeErr := ch.Output.Write([]byte(res))
-	if writeErr != nil 
-	{
+	if writeErr != nil {
 		return writeErr
 	}
 
 	res, computeErr := PostfixExpressionCalculator(e)
-	if computeErr != nil 
-	{
+	if computeErr != nil {
 		return computeErr
 	}
 
