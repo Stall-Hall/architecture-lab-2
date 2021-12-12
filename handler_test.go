bb343f7a8bfed3e9877aae49556f93f773904de6
suhanovamasha21@gmail.com <suhanovamasha21@gmail.com>
2021-12-12 16:44:18 +0200

    Fix handler tests

--- handler_test.go
+++ handler_test.go
@@ -13,22 +13,32 @@ type HandlerSuite struct{}
 
 var _ = Suite(&HandlerSuite{})
 
-func (s *HandlerSuite) TestingUseResultForOutput(c *C) {
-	output := bytes.NewBufferString("")
-	handler := ComputeHandler{
-		Input:  strings.NewReader("2 5 -"),
-		Output: output,
-	}
-	err := handler.Compute()
-	c.Assert(err, IsNil)
-	c.Assert(output.String(), Equals, "-3\n")
-}
-
-func (s *HandlerSuite) TestingBackError(c *C) {
-	handler := ComputeHandler{
-		Input:  strings.NewReader("New text is read"),
+func (s *HandlerSuite) TestWrongTypeOfInput(c *C) {
+	handler := ComputeHandler {
+		Input:  strings.NewReader("Letters are not numbers"),
 		Output: bytes.NewBufferString(""),
 	}
 	err := handler.Compute()
-	c.Assert(err.Error(), Equals, "invalid input expression")
+	c.Assert(err.Error(), Equals, "Wrong type of input")
+}
+
+func (s *HandlerSuite) TestWrongNumberOfNumbers(c *C) {
+	handler := ComputeHandler {
+		Input: strings.NewReader("2 3 7 +"),
+		Output: bytes.NewBufferString(""),
+	}
+	err := handler.Compute()
+	c.Assert(err.Error(), Equals, 
+	"Wrong number of input numbers im expression")
+}
+
+func (s *HandlerSuite) TestHandlerComputeInput(c *C) {
+	handler := ComputeHandler {
+		Input: strings.NewReader("1 7 +"),
+		Output: bytes.NewBufferString(""),
+	}
+	output := bytes.NewBufferString("")
+	err := handler.Compute()
+	c.Assert(err, isNil)
+	c.Assert(output.String(), Equals, "+ 1 7")
 }
